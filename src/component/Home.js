38b7e299a873c812e03a5e6cbfcba6e3f8caebf6
shakti <shaktiyadav635@gmail.com>
2024-10-06 20:29:06 +0530

    s

--- src/component/Home.js
+++ src/component/Home.js
@@ -17,59 +17,17 @@ const Home= () => {
     location:'',
    
   });
-   // State for form submission status
-   const [submitted, setSubmitted] = useState(false);
+  
+  
 
-   // State for form validation errors
-   const [formErrors, setFormErrors] = useState({});
- 
-   // Handle form field changes
-   const handleChange = (e) => {
-     setFormData({
-       ...formData,
-       [e.target.name]: e.target.value,
-     });
-   };
- 
-   // Validate the form fields
-   const validateForm = () => {
-     const errors = {};
-     if (!formData.name) errors.name = "Name is required";
-     if (!formData.contact) errors.contact = "Contact is required";
-     if (formData.contact && formData.contact.length !== 10)
-       errors.contact = "Contact must be 10 digits";
-     if (!formData.email) errors.email = "Email is required";
-     if (!formData.location) errors.location = "Location is required";
-     if (!formData.gender) errors.gender = "Gender is required";
-     if (!formData.service) errors.service = "Service is required";
-     if (!formData.date) errors.date = "Date is required";
-     if (!formData.time) errors.time = "Time is required";
- 
-     return errors;
-   };
- 
-   // Handle form submission
-   const handleSubmit = (e) => {
-     e.preventDefault();
-     const { name, value } = e.target;
-     setFormData({
-       ...formData,
-       [name]: value,
-     });
- 
-     const errors = validateForm();
-     if (Object.keys(errors).length > 0) {
-       setFormErrors(errors); // If there are errors, display them
-     } else {
-       // If no errors, process form submission
-       console.log("Form Data Submitted:", formData);
- 
-       // Here, you can add email sending logic via emailjs or another service
- 
-       setSubmitted(true); // Set the form as submitted
-     }
-   };
- 
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   // Sending email through EmailJS
   const sendEmail = (e) => {
@@ -181,12 +139,7 @@ const insta = () => {
             Let's not wait for the "Perfect Look"
           </h5>
           <p className="text-center">Book An Appointment Now!</p>
-          {submitted ? (
-            <div className="success-message">
-              <h3>Appointment Successfully Booked!</h3>
-              <p>Thank you, {formData.name}. We will contact you soon.</p>
-            </div>
-          ) : (
+          (
       <form onSubmit={onAppointmentClick}>
         <div className='form'>
         <div className='ph1'>
@@ -260,7 +213,7 @@ const insta = () => {
         <button type="submit" className="btn-submit">Send Appointment</button>
       </form>
     
-          )}
+          )
           </div>
     </div>
     </section>
