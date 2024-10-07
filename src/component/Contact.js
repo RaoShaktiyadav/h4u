78f0b7ac49558c892a47459040ba7c7627311f44
shakti <shaktiyadav635@gmail.com>
2024-10-07 17:40:05 +0530

    s

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -267,4 +267,3 @@ const Contact = () => {
   )
 }
 export default Contact;
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3529.2816626043946!2d76.63951887530422!3d27.801099776128893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3972bb89f9bafaf7%3A0x8ddd8c3983a5859a!2sCut%20N%20Curl%20Khairthal!5e0!3m2!1sen!2sin!4v1728298175301!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
